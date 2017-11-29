5a00c5ff1e622905ecf938ba3cbe1e252dfcf870
Brian Oxley <boxley@thoughtworks.com>
2017-11-29 15:29:44 -0600

    Types

--- josephus.py
+++ josephus.py
@@ -4,82 +4,6 @@
 """
 
 
-class RebelsIterator:
-    def __init__(self, rebels):
-        def _max():
-            curr = rebels.curr
-            while curr.right > curr:
-                curr = curr.right
-            return curr
-
-        self.first = _max().right
-        self.curr = self.first
-        self.repeated = False
-
-    def __next__(self):
-        if self.curr == self.first:
-            if self.repeated:
-                raise StopIteration
-            else:
-                self.repeated = True
-        curr = self.curr
-        self.curr = curr.right
-        return curr
-
-
-class Rebels:
-    def __init__(self, count):
-        """
-        >>> Rebels(1)
-        [1]
-        >>> Rebels(2)
-        [1, 2]
-        >>> Rebels(3)
-        [1, 2, 3]
-        >>> Rebels(4)
-        [1, 2, 3, 4]
-        >>> Rebels(5)
-        [1, 2, 3, 4, 5]
-        """
-        self.curr = Soldier.one()
-        curr = self.curr
-        while 1 < count:
-            curr = curr.join()
-            count -= 1
-
-    def __iter__(self):
-        return RebelsIterator(self)
-
-    def disband(self):
-        """
-        >>> Rebels(1).disband()
-        [1]
-        >>> Rebels(2).disband()
-        [1, 2]
-        >>> Rebels(3).disband()
-        [1, 2]
-        >>> Rebels(4).disband()
-        [1, 4]
-        >>> Rebels(5).disband()
-        [2, 4]
-        """
-        while True:
-            victim = self.curr.right.right
-            if victim == self.curr:
-                return self
-            self.curr = victim.right
-            victim.leave()
-
-    def __repr__(self):
-        """
-        >>> x = Rebels(4)
-        >>> x.curr = x.curr.left
-        >>> x
-        [1, 2, 3, 4]
-        """
-        return str(list(map(lambda s: s.num, self)))
-
-
 class Soldier:
     @staticmethod
     def one():
@@ -88,12 +12,13 @@ class Soldier:
         >>> solo.num
         1
         """
+        # noinspection PyTypeChecker
         solo = Soldier(1, None, None)
         solo.left = solo
         solo.right = solo
         return solo
 
-    def __init__(self, num, left, right):
+    def __init__(self, num: int, left: 'Soldier', right: 'Soldier'):
         self.num = num
         self.left = left
         self.right = right
@@ -132,6 +57,82 @@ class Soldier:
     def __repr__(self):
         return "{}\\{}/{}".format(
             self.left.num, self.num, self.right.num)
+
+
+class Rebels:
+    def __init__(self, count: int):
+        """
+        >>> Rebels(1)
+        [1]
+        >>> Rebels(2)
+        [1, 2]
+        >>> Rebels(3)
+        [1, 2, 3]
+        >>> Rebels(4)
+        [1, 2, 3, 4]
+        >>> Rebels(5)
+        [1, 2, 3, 4, 5]
+        """
+        self.curr = Soldier.one()
+        curr = self.curr
+        range(1, count)
+        for _ in range(1, count):
+            curr = curr.join()
+
+    def __iter__(self):
+        return RebelsIterator(self)
+
+    def disband(self):
+        """
+        >>> Rebels(1).disband()
+        [1]
+        >>> Rebels(2).disband()
+        [1, 2]
+        >>> Rebels(3).disband()
+        [1, 2]
+        >>> Rebels(4).disband()
+        [1, 4]
+        >>> Rebels(5).disband()
+        [2, 4]
+        """
+        while True:
+            victim = self.curr.right.right
+            if victim == self.curr:
+                return self
+            self.curr = victim.right
+            victim.leave()
+
+    def __repr__(self):
+        """
+        >>> x = Rebels(4)
+        >>> x.curr = x.curr.left
+        >>> x
+        [1, 2, 3, 4]
+        """
+        return str(list(map(lambda s: s.num, self)))
+
+
+class RebelsIterator:
+    def __init__(self, rebels: Rebels):
+        def _max() -> Soldier:
+            curr = rebels.curr
+            while curr.right > curr:
+                curr = curr.right
+            return curr
+
+        self.first = _max().right
+        self.curr = self.first
+        self.repeated = False
+
+    def __next__(self):
+        if self.curr == self.first:
+            if self.repeated:
+                raise StopIteration
+            else:
+                self.repeated = True
+        curr = self.curr
+        self.curr = curr.right
+        return curr
 
 
 if __name__ == "__main__":
