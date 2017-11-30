11502232b1650a6a83cc75f18b341ef88c008829
Brian Oxley <boxley@thoughtworks.com>
2017-11-30 08:18:06 -0600

    Tidy

--- setup.py
+++ setup.py
@@ -60,17 +60,11 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    packages=find_packages(),
 
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #   py_modules=["my_module"],
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[],
+    package_data={
+        '': ['*.md'],
+    },
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
